a1d12ee6544f5a22011f5cee7df480dde2170f20
Eric <eric.ucl@outlook.com>
2023-11-27 14:15:17 -0800

    alter dark mode File AuthDebugger.jsx

--- movies-app/client/src/components/AuthDebugger.jsx
+++ movies-app/client/src/components/AuthDebugger.jsx
@@ -11,18 +11,24 @@ export default function AuthDebugger() {
         return Object.keys(userInfo).map(key => `${key}: ${userInfo[key]}`).join('\n');
     };
 
+    const darkModeStyles = {
+        backgroundColor: '#343a40', // Dark background
+        color: '#f8f9fa', // Light text
+        borderColor: '#495057' // Dark border
+    };
+
     return (
-        <div className="container mt-4">
-            <div className="card mb-3">
+        <div className="container mt-4" style={{ backgroundColor: '#212529', color: 'white' }}>
+            <div className="card mb-3" style={darkModeStyles}>
                 <div className="card-header">Access Token</div>
                 <div className="card-body">
-                    <pre className="bg-light p-2 border">{JSON.stringify(accessToken, null, 2)}</pre>
+                    <pre className="p-2" style={darkModeStyles}>{JSON.stringify(accessToken, null, 2)}</pre>
                 </div>
             </div>
-            <div className="card">
+            <div className="card" style={darkModeStyles}>
                 <div className="card-header">User Info</div>
                 <div className="card-body">
-                    <pre className="bg-light p-2 border">{formatUserInfo(user)}</pre>
+                    <pre className="p-2" style={darkModeStyles}>{formatUserInfo(user)}</pre>
                 </div>
             </div>
         </div>
